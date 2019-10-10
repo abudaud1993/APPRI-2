bc3ad6a68a6840d8e95d1360f106b3bf86ba12eb
abudaud1993 <abudaud1993@gmail.com>
2019-10-10 11:06:48 +0600

    Appri PSD HTML Build FILE ADDED

---
+++ js/main.js
@@ -0,0 +1,11 @@
+(function ($) {
+
+
+
+    // data - background - img
+    $("[data-background]").each(function () {
+        $(this).css("background-image","url(" + $(this).attr("data-background") + ")");
+    })
+
+
+})(jQuery);
