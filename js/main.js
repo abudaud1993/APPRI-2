5005d0b140e9be3d2d3299feb1ae6159b1404928
abudaud <abudaud1994@gmail.com>
2019-10-11 18:15:23 +0600

    more added

--- js/main.js
+++ js/main.js
@@ -5,7 +5,14 @@
     // data - background - img
     $("[data-background]").each(function () {
         $(this).css("background-image","url(" + $(this).attr("data-background") + ")");
-    })
+    });
+
+
+    $('.counter').counterUp({
+        delay: 10,
+        time: 19000,
+      });
+     
 
 
 })(jQuery);
